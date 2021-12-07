65892442a7e799bdcbaba88515371819bd70d6d3
Stoneberg <timmy.stoneberg@steelseries.com>
2021-12-07 06:25:17 -0600

    Initial commit.

---
+++ hello_world.py
@@ -0,0 +1,6 @@
+class HelloWorld:
+    def __init__(self):
+        print('Creating Hello World Class')
+
+    def hello_world(self):
+        print("Hello World")
---
+++ main.py
@@ -0,0 +1,9 @@
+# This is a sample Python script.
+
+from hello_world import HelloWorld
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    HelloWorld().hello_world()
+
+
