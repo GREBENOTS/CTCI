40fed48a3c7d574b79229a326c5ea0edb0b76938
Stoneberg <timmy.stoneberg@steelseries.com>
2021-12-08 10:57:13 -0600

    Added 1.4 skeleton.

---
+++ chapter_1/One_Four.py
@@ -0,0 +1,25 @@
+"""
+Palindrome Permutation:  Given a string, write a function to check if it is a permutation of a palindrome.  A palindrome
+is a rearrangement of letters.  The palindrome is a word or phrase that is the same forwards and backwards.
+A permutation is a rearrangement of letters.  The palindrome does not need to be limited to just dictionary words.
+You can ignore casing and non-letter characters.
+
+Input:  Tact Coa
+Output:  True (permutations:  "taco cat", "atco cta", etc.)
+"""
+
+
+class PalindromePermutation:
+    def __init__(self):
+        self.target_string = ''
+
+    def is_palindrome(self, s):
+        pass
+
+    def is_permutation(self):
+        # maybe?
+        pass
+
+
+
+
