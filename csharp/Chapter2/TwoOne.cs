b6692c77e36a3ea3c4b428965a5c83bda0588833
Stoneberg <timmy.stoneberg@steelseries.com>
2021-12-14 08:01:29 -0600

    Added 2.1 skeleton.

---
+++ csharp/Chapter2/TwoOne.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace csharp.Chapter2 {
+    /// Remove Dups:  Write code to remove duplicated from an unsorted linked list.
+    
+    public class TwoOne {
+        public TwoOne() {
+            Console.WriteLine("Question 2.1");
+        }
+    }
+}
--- csharp/Program.cs
+++ csharp/Program.cs
@@ -1,12 +1,11 @@
 ﻿using System;
+using csharp.Chapter2;
 
-namespace csharp
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+namespace csharp {
+    class Program {
+        static void Main(string[] args) {
             Console.WriteLine("Hello World!");
+            TwoOne twoOne = new();
         }
     }
 }
