5690119c5e5f42456128ae3a1628b9566d45bb78
Stoneberg <timmy.stoneberg@steelseries.com>
2021-12-14 08:14:33 -0600

    Added 2.1 simple linked list.

---
+++ csharp/Chapter2/Node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace csharp.Chapter2 {
+    public class Node {
+        private Node next = null;
+        private int data;
+
+        public Node(int d) {
+            this.data = d;
+        }
+
+        private void AppendToTail(int d) {
+            Node end = new(d);
+            Node n = this;
+            while(n.next != null) {
+                n = n.next;
+            }
+            n.next = end;
+        }
+    }
+}
